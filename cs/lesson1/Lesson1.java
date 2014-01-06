7e25966a83356cf2ec3a998667750b84ac63d71d
Spyobird <elliotlimzy@gmail.com>
2014-01-06 19:34:10 +0800

    Lesson 1

--- cs/lesson1/Lesson1.java
+++ cs/lesson1/Lesson1.java
@@ -7,9 +7,10 @@ public class Lesson1
 	public static void main(String[] args) throws IOException
 	{
 		String name;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("Please enter your name: ");
-		name = br.readLine();
-		System.out.println("Hello World " + name + "!");
+		name = kb.readLine();
+		System.out.println("Hello World, dear " + name + "!");
+		System.exit(0);
 	}
 }
